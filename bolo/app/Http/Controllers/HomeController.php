9d5f5b1702a2ab99e54013658165c69b77dd1816
David Caulton <davidcaulton@CLSLC6440DavidCaulton.local>
2018-03-26 19:17:09 -0500

    develop
    Add dropdown for creating new nodes.
    Add object to handle output feed

--- bolo/app/Http/Controllers/HomeController.php
+++ bolo/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+#        dd(Auth::user()['email']);
+#        dd(Auth::user());
         return view('home');
     }
 }
--- bolo/resources/views/home.blade.php
+++ bolo/resources/views/home.blade.php
@@ -3,6 +3,38 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
+    <div class='col-md-8'>
+        <div>
+            <button class='btn btn-primary dropdown-toggle' type="button" data-toggle="dropdown">New
+                <span class="caret"></span>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Feed</a></li>
+                    <li><a href="#">Transformer</a></li>
+                    <li><a href="#">Api</a></li>
+                </ul>
+            </button/>
+        </div>
+
+        <div class="float-right">
+            <h2>processing layout</h2>
+            <div id='processing-nodes'>
+            </div>
+        </div>
+	</div>
+ 
+
+    <div class='col-md-4'>
+    	Output
+        <div>
+            <object id="output-panel"
+                    type="text/html"
+                    data="">
+            (output goes here)
+            </object>
+        </div>
+    </div>
+<!--
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
@@ -18,6 +50,8 @@
                 </div>
             </div>
         </div>
+-->
+
     </div>
 </div>
 @endsection
